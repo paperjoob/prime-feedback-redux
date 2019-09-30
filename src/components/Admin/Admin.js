782093f55d409d85f2d6e96a39161b8af2e01678
See Yang <seeyang24@gmail.com>
2019-09-30 10:58:31 -0500

    added adminlist to admin in order to display server data

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -6,10 +6,6 @@ import AdminList from '../AdminList/AdminList';
 
 class Admin extends Component {
 
-    state = {
-        feedback: []
-    }
-
     componentDidMount = () => {
         this.getFeedback();
     }
@@ -17,9 +13,10 @@ class Admin extends Component {
     getFeedback = () => {
         Axios.get('/api/feedback')
         .then( (response) => {
-            this.setState({
-                feedback: response.data
-            });
+            this.props.dispatch({
+              type: 'GRAB_FEEDBACK',
+              payload: response.data
+            })
             console.log(this.state.feedback)
           }).catch( (error) => {
             console.log('error in GET', error);
@@ -28,15 +25,6 @@ class Admin extends Component {
 
 
   render() {
-
-    const displayFeed = this.state.feedback.map((feed, i) => {
-        return <tr key={i}><td>{feed.feeling}</td>
-        <td> {feed.understanding}</td>
-        <td>{feed.support}</td>
-        <td>{feed.comments}</td>
-        <td><button>Delete</button></td>
-        </tr>
-    })
       
     return (
       <div className="AdminDiv">
@@ -44,19 +32,7 @@ class Admin extends Component {
             <h1>Admin</h1>
         </header>
         <div>
-            <table className="AdminTable">
-                <tbody>
-                    <tr>
-                        <th>Feeling</th>
-                        <th>Understanding</th>
-                        <th>Support</th>
-                        <th>Comments</th>
-                        <th>Delete</th>
-                    </tr>
-                        {displayFeed}
-                </tbody>
-            </table>
-            {/* <AdminList getFeedback={this.getFeedback}/> */}
+            <AdminList />
         </div>
       </div>
     );
--- src/components/AdminList/AdminList.js
+++ src/components/AdminList/AdminList.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 class AdminList extends Component {
 
   render() {
 
-    const displayFeed = this.props.reduxState.feedbackList.map((feed, i) => {
-        return <tr key={i}><td>{feed.feeling}</td>
-        <td> {feed.understanding}</td>
-        <td>{feed.support}</td>
-        <td>{feed.comments}</td>
-        <td><button>Delete</button></td>
-        </tr>
+        const displayFeed = this.props.reduxState.feedbackList.map((feed, i) => {
+        return <tr key={i}>
+                <td>{feed.feeling}</td>
+                <td> {feed.understanding}</td>
+                <td>{feed.support}</td>
+                <td>{feed.comments}</td>
+                <td><button>Delete</button></td>
+            </tr>
     })
-      
+
     return (
-      <div className="AdminListDiv">
-            <table className="AdminTable">
+      <div>
+        <table className="AdminTable">
                 <tbody>
-                    <tr>
-                        <th>Feeling</th>
-                        <th>Understanding</th>
-                        <th>Support</th>
-                        <th>Comments</th>
-                        <th>Delete</th>
-                    </tr>
-                        {displayFeed}
-                </tbody>   
-            </table>
+                     <tr>
+                         <th>Feeling</th>
+                         <th>Understanding</th>
+                         <th>Support</th>
+                         <th>Comments</th>
+                         <th>Delete</th>
+                     </tr>
+                         {displayFeed}
+                 </tbody>   
+             </table>
       </div>
     );
-}
+  }
 }
 
  // export the feedbackReduxState via connect
